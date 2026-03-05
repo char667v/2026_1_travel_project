d297a1001c39098d0b91234a7fd371863e802739
Char <143526767+char667v@users.noreply.github.com>
2026-03-05 09:45:44 +0100

    tilføjet email og password

--- x.py
+++ x.py
@@ -53,3 +53,22 @@ def validate_user_last_name():
     if not re.match(REGEX_USER_LAST_NAME, user_last_name):
         raise Exception("company_exception user_last_name")
     return user_last_name
+
+##############################
+# validating email
+REGEX_USER_EMAIL = "^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$"
+def validate_user_email():
+    user_email = request.form.get("user_email", "").strip()
+    if not re.match(REGEX_USER_EMAIL, user_email):
+        raise Exception("company_exception user_email")
+    return user_email
+
+##############################
+USER_PASSWORD_MIN = 8
+USER_PASSWORD_MAX = 50
+REGEX_PASSWORD = f"^.{{{USER_PASSWORD_MIN},{USER_PASSWORD_MAX}}}$"
+def validate_user_password():
+    user_password = request.form.get("user_password", "").strip()
+    if not re.match(REGEX_USER_LAST_NAME, user_password):
+        raise Exception("company_exception user_password")
+    return user_password
